e430ec7201a6dff6e5488f87ce151ac41e5a6e47
Jefersonsteven <gfreycode@gmail.com>
2026-03-21 09:51:12 -0500

    server run

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 // Importamos las dependencias necesarias
-import express from 'express'; // Framework para crear el servidor
-import dotenv from 'dotenv'; // Carga de variables de entorno
+import express from "express"; // Framework para crear el servidor
+import dotenv from "dotenv"; // Carga de variables de entorno
 
 // Cargamos las variables de entorno desde el archivo .env.local
-dotenv.config({ path: '.env.local' });
+dotenv.config({ path: ".env.local" });
+
+const {
+  API_VERSION,
+  WHATSAPP_BUSINESS_PHONE_NUMBER_ID,
+  ACCESS_TOKEN,
+  WHATSAPP_USER_PHONE_NUMBER,
+  WEBHOOK_VERIFY_TOKEN,
+  PORT
+} = process.env;
 
 // Creamos una instancia de Express
 const app = express();
@@ -12,15 +21,22 @@ const app = express();
 app.use(express.json());
 
 // Obtenemos el puerto y el token de verificación de las variables de entorno
-const port = process.env.PORT || 3000;
-const verifyToken = process.env.ACCESS_TOKEN;
+const port = PORT || 3000;
+const verifyToken = WEBHOOK_VERIFY_TOKEN || "my_verify_token";
 
 // Ruta para manejar las solicitudes GET (verificación del webhook)
-app.get('/', (req, res) => {
-  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
+app.get("/", (req, res) => {
+  const {
+    "hub.mode": mode,
+    "hub.challenge": challenge,
+    "hub.verify_token": token,
+  } = req.query;
 
-  if (mode === 'subscribe' && token === verifyToken) {
-    console.log('WEBHOOK VERIFIED');
+  console.log('x', token, mode, challenge);
+  
+
+  if (mode === "subscribe" && token === verifyToken) {
+    console.log("WEBHOOK VERIFIED");
     res.status(200).send(challenge);
   } else {
     res.status(403).end();
@@ -28,8 +44,8 @@ app.get('/', (req, res) => {
 });
 
 // Ruta para manejar las solicitudes POST
-app.post('/', (req, res) => {
-  const timestamp = new Date().toISOString().replace('T', ' ').slice(0, 19);
+app.post("/", (req, res) => {
+  const timestamp = new Date().toISOString().replace("T", " ").slice(0, 19);
   console.log(`\n\nWebhook received ${timestamp}\n`);
   console.log(JSON.stringify(req.body, null, 2));
   res.status(200).end();
